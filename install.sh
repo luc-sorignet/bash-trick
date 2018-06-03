6c02364cf1bdf263839b8dbb5d50906946c37c75
Luc SORIGNET <luc.sorignet@gmail.com>
2018-06-03 11:08:43 +0200

    * Modification des configuration

--- install.sh
+++ install.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DOTFILE_DIR=$DIR/dotfiles
 cp -r $DIR/bash/.bash ~/
-cp $DIR/bash/.bashrc ~/ 
+cp $DIR/bash/.bashrc ~/
 
 
 if [[ -x "$(command -v zsh)" ]]; then
     cp $DIR/zsh/.zshrc ~/.zshrc
     if [[ -d ~/.oh-my-zsh ]]; then
-	cp $DIR/zsh/lso.zsh-theme ~/.oh-my-zsh/themes/
+        cp $DIR/zsh/lso.zsh-theme ~/.oh-my-zsh/themes/
     fi
-   
 fi
 
-cp -r $DOTFILE_DIR/.vim ~/
-cp $DOTFILE_DIR/.vimrc ~/.vimrc
+cp -r $DOTFILE_DIR/. ~/
 
-if [[ -x "$(command -v git)" ]]; then
-	cp $DOTFILE_DIR/.gitconfig ~/
-	cp $DOTFILE_DIR/.gitmessage ~/
-fi
 vim +PluginInstall +qall
 echo "install sucessfull you can restart your terminal !"
