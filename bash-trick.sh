b9b2ae9c452c2b14a6cdc4fd93b95d3c6fc81ea2
Luc SORIGNET <luc.sorignet@gmail.com>
2019-02-17 19:10:31 +0100

    * change setup v2 * add rtfm function

---
+++ bash-trick.sh
@@ -0,0 +1,29 @@
+function reload_cnf() {
+if [ -d $BASHTRICK_DIR ]; then
+    if [ -d $BASHTRICK_DIR/aliases ]; then
+        for f in $BASHTRICK_DIR/aliases/*.alias ; do
+            # loading aliases if exist
+            if [ -f $f ]; then
+            source $f
+            fi
+        done
+    fi
+    if [ -d $BASHTRICK_DIR/functions ]; then
+        for f in $BASHTRICK_DIR/functions/*.function ; do
+            # loading function if exist
+            if [ -f $f ]; then
+            source $f
+            fi
+        done
+    fi
+    if [ -d $BASHTRICK_DIR/config ]; then
+        for f in $BASHTRICK_DIR/config/*.conf ; do
+            # loading conf if exist
+            if [ -f $f ]; then
+            source $f
+            fi
+        done
+    fi
+fi
+return 0
+}
--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
+export BASHTRICK_DIR="$HOME/.bash-trick"
+
+source $BASHTRICK_DIR/bash-trick.sh
+
 RCFILE=(".bashrc" ".zshrc")
-VAR=$(cat <<'EOF'
-        export BASHTRICK_DIR="$HOME/.bash-trick"
-        source $BASHTRICK_DIR/bt.sh
-        USR_BIN="$HOME/bin"
-        if [ -d $USR_BIN ]; then
-            export PATH="$PATH:$USR_BIN"
-        fi
-        export FPATH=$BASHTRICK_DIR/functions:$FPATH
+
+VAR=$(cat <<- EOF
+# BASHTRICK >>
+    export BASHTRICK_DIR="$HOME/.bash-trick"
+    source $BASHTRICK_DIR/bash-trick.sh
+    USR_BIN="$HOME/bin"
+    if [ -d $USR_BIN ]; then
+        export PATH="$PATH:$USR_BIN"
+    fi
+    export FPATH=$BASHTRICK_DIR/functions:$FPATH
+# << BASHTRICK
 EOF
 )
 
-
-
+echo "setup bash-trick in .bashrc .zshrc files"
 for F in "${RCFILE[@]}";
 do
     FILE=$HOME/$F
@@ -27,6 +33,4 @@ do
         fi
     fi
 done
-
-source $HOME/.bash-trick/bt.sh
 reload_cnf
